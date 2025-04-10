b5359413a78bc53829855775a1299a044153ac4a
brittLiban <britt.liban@student.greenriver.edu>
2025-04-09 21:11:22 -0700

    Done!

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -23,14 +23,27 @@ public class ExplorerSearchTest {
     @Test
     public void testReachableArea_noMoves() {
         int[][] island = {
-            {1, 1, 1, 1, 1},
-            {1, 3, 3, 3, 1},
-            {1, 3, 0, 3, 1},
-            {1, 3, 3, 3, 1},
-            {1, 1, 1, 1, 1}
+                { 1, 1, 1, 1, 1 },
+                { 1, 3, 3, 3, 1 },
+                { 1, 3, 0, 3, 1 },
+                { 1, 3, 3, 3, 1 },
+                { 1, 1, 1, 1, 1 }
         };
         int actual = ExplorerSearch.reachableArea(island);
         assertEquals(1, actual);
+    }
+
+    @Test
+    public void testReachableArea_surroundedByMountains() {
+        int[][] island = {
+                { 1, 1, 1, 1, 1 },
+                { 1, 3, 3, 3, 1 },
+                { 1, 3, 0, 3, 1 },
+                { 1, 3, 3, 3, 1 },
+                { 1, 1, 1, 1, 1 }
+        };
+
+        assertEquals(1, ExplorerSearch.reachableArea(island));
     }
 
     // Add more tests here!
